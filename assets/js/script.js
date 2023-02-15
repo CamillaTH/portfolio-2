946436c1207ac6ff3ec11f7060afa0b2e1b0c145
CamillaTH <hiltulacamilla@gmail.com>
2023-02-15 09:15:47 +0000

    added html for score and declared js constants

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+/* declare elements constants */
+const rock = document.getElementById("rock-btn");
+const paper = document.getElementById("paper-btn");
+const scissors = document.getElementById("scissors-btn");
+const yourPoints = document.getElementById("your-points-text").innerText;
+const computerPoints = document.getElementById("computer-points-text").innerText;
+const yourChoice = document.getElementById("your-choice-text").innerText;
+const computerChoice = document.getElementById("computer-choice-text").innerText;
